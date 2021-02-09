33b66ba4c1117c4615eff5914e7b38df019b47c1
Álex Law <junior-302@hotmail.com>
2021-02-08 23:05:14 -0300

    [ADD] #0000 - criando skeleton para projetos em Angular

---
+++ skeleton-front-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MenuModule } from './core/components/menu/menu.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    MenuModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
